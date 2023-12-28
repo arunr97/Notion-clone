c48fcf4ac6d204616e08b7b1d971568f4d24be47
Arun Rajbhar <arunrajbhar.97@gmail.com>
2023-12-28 19:00:51 +0530

    testing git config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  console.log('db is ', db);
   return (
     <html lang="en">
       <body className={inter.className}>
