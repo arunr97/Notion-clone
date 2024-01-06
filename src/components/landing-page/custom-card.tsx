e50f5ed3ddaae9b732fda4d049e39ab45a2e183c
Arun Rajbhar <arunrajbhar.97@gmail.com>
2024-01-07 02:55:51 +0530

    Style: Converted single Quotes to double qoutes Chore: Landing page changes- Added avatar

--- src/components/landing-page/custom-card.tsx
+++ src/components/landing-page/custom-card.tsx
@@ -11,15 +11,21 @@ type CustomCardProps = CardProps & {
 
 const CustomCard: React.FC<CustomCardProps> = ({
   className,
-  cardContent,
   cardHeader,
+  cardContent,
   cardFooter,
   ...props
 }) => {
   return (
     <Card className={cn('w-[380px]', className)} {...props}>
       <CardHeader>{cardHeader}</CardHeader>
-      <CardContent className='grid gap-4'>{cardContent}</CardContent>
+      <CardContent
+        className='grid
+        gap-4
+      '
+      >
+        {cardContent}
+      </CardContent>
       <CardFooter>{cardFooter}</CardFooter>
     </Card>
   );
